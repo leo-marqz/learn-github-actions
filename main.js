89eb6f711accb34d5c9e38cb0c1c64b794cbe73f
Elmer L. Marquez <62399619+leo-marqz@users.noreply.github.com>
2025-07-19 21:06:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 console.log("START...");
+
 console.log('Ejecutando codijo JS en main.js desde un workflow de github actions');
 console.log("Variable de ambiente configurada en el workflow MI_VARIABLE -> ", process.env.MI_VARIABLE);
+console.log(`Variable Centralizada: ${process.env.VARIABLE_CENTRALIZADA}`);
+console.log(`Secreto Centralizado: ${process.env.SECRETO_CENTRALIZADO}`);
+
 console.log("END...");
