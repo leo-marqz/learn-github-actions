73ea2059d8b434acb1d7911068f1c1f4483124ec
Elmer L. Marquez <62399619+leo-marqz@users.noreply.github.com>
2025-07-19 19:47:33 -0600

    Create main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("START...");
+console.log('Ejecutando codijo JS en main.js desde un workflow de github actions');
+console.log("Variable de ambiente configurada en el workflow MI_VARIABLE -> ", process.env.MI_VARIABLE);
+console.log("END...");
